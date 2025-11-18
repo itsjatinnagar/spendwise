c03d0bdc1856c3441779461a8c55cfbba7405175
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2025-11-18 07:54:36 +0530

    Migrate to Expo 54

--- src/utils/uuid.ts
+++
@@ -1,6 +0,0 @@
-export const uuid = () => {
-  return "xxxxxxxx-xxxx-4xxx-xxxx-xxxxxxxxxxxx".replace(/[x]/g, (value) => {
-    const random = Math.floor(Math.random() * 16);
-    return random.toString(16);
-  });
-};
