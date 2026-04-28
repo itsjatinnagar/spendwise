c7bf3418c2d0024cefd554ac173037cb0dc817db
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2026-04-28 19:55:26 +0530

    refactor: extract component, broaden query invalidation

--- src/app/transactions/review.tsx
+++ src/app/transactions/review.tsx
@@ -4,7 +4,7 @@ import Form from "@/components/common/form";
 import FormField from "@/components/common/form-field";
 import Input from "@/components/common/input";
 import Text from "@/components/common/text";
-import { CategoryType, ParsedTxnStatus } from "@/database/schema";
+import { CategoryType, parsedTxns, ParsedTxnStatus } from "@/database/schema";
 import { useCategories } from "@/hooks/use-categories";
 import { useKeyboardHeight } from "@/hooks/use-keyboard-height";
 import { useParsedTxn } from "@/hooks/use-parsed-txn";
@@ -37,18 +37,40 @@ const STATUSES = Object.values(ParsedTxnStatus).filter(
 
 export default function Screen() {
   const { id } = useLocalSearchParams<{ id: string }>();
-  const { data: txn } = useParsedTxn(id);
-  const { data } = useCategories();
-  const { isPending, mutateAsync } = useUpdateParsedTxn();
-  const [state, setState] = useState<State>(() => ({
-    categoryId: txn?.categoryId ?? "",
-    note: txn?.note ?? "",
-    status: txn?.status ?? ParsedTxnStatus.ORIGINAL,
-  }));
-  const [height, setHeight] = useState(48);
+  const { data, isLoading } = useParsedTxn(id);
+
   const keyboardHeight = useKeyboardHeight();
 
-  if (!txn) return;
+  if (isLoading || !data) return;
+
+  return (
+    <SafeAreaView style={styles.container} edges={["bottom", "left", "right"]}>
+      <KeyboardAvoidingView style={styles.wrapper} behavior={undefined}>
+        <ScrollView
+          style={{ marginBottom: keyboardHeight }}
+          keyboardShouldPersistTaps="handled"
+          showsVerticalScrollIndicator={false}
+        >
+          <Children txn={data} />
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
+
+type Props = {
+  txn: typeof parsedTxns.$inferSelect;
+};
+function Children({ txn }: Props) {
+  console.log(txn);
+  const { data } = useCategories();
+  const { isPending, mutateAsync } = useUpdateParsedTxn();
+  const [state, setState] = useState<State>({
+    categoryId: txn.categoryId ?? "",
+    note: txn.note ?? "",
+    status: txn.status,
+  });
+  const [height, setHeight] = useState(48);
 
   const TYPE = txn.amount < 0 ? CategoryType.EXPENSE : CategoryType.INCOME;
   const categories = data?.filter((val) => val.type === TYPE);
@@ -73,73 +95,64 @@ export default function Screen() {
   };
 
   return (
-    <SafeAreaView style={styles.container} edges={["bottom", "left", "right"]}>
-      <KeyboardAvoidingView style={styles.wrapper} behavior={undefined}>
-        <ScrollView
-          style={{ marginBottom: keyboardHeight }}
-          keyboardShouldPersistTaps="handled"
-          showsVerticalScrollIndicator={false}
-        >
-          <Form>
-            <FormField>
-              <Text.Label>Amount</Text.Label>
-              <Input value={formatAmount(txn.amount)} readOnly />
-            </FormField>
-            <FormField>
-              <Text.Label>Description</Text.Label>
-              <Input
-                value={txn.description}
-                onContentSizeChange={handleSizeChange}
-                style={{ height }}
-                multiline
-                readOnly
-              />
-            </FormField>
-            <FormField>
-              <Text.Label>Date</Text.Label>
-              <Input value={formatDate(txn.timestamp)} readOnly />
-            </FormField>
-            <FormField>
-              <Text.Label>Category</Text.Label>
-              <View style={styles.row}>
-                {categories?.map((val) => (
-                  <Chip
-                    key={val.id}
-                    label={val.name}
-                    selected={val.id === state.categoryId}
-                    callback={() => handleChange("categoryId", val.id)}
-                  />
-                ))}
-              </View>
-            </FormField>
-            <FormField>
-              <Text.Label>Note</Text.Label>
-              <Input
-                placeholder="Additional Note"
-                value={state.note}
-                onChangeText={(text) => handleChange("note", text)}
-              />
-            </FormField>
-            <FormField>
-              <Text.Label>Status</Text.Label>
-              <View style={styles.row}>
-                {STATUSES.map((status) => (
-                  <Chip
-                    key={status}
-                    label={parsedTxnStatusLabel(status)}
-                    selected={state.status === status}
-                    callback={() => handleChange("status", status)}
-                  />
-                ))}
-              </View>
-            </FormField>
-            <Button onPress={handleSubmit} disabled={isPending}>
-              <Button.Label>Save</Button.Label>
-            </Button>
-          </Form>
-        </ScrollView>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+    <Form>
+      <FormField>
+        <Text.Label>Amount</Text.Label>
+        <Input value={formatAmount(txn.amount)} readOnly />
+      </FormField>
+      <FormField>
+        <Text.Label>Description</Text.Label>
+        <Input
+          value={txn.description}
+          onContentSizeChange={handleSizeChange}
+          style={{ height }}
+          multiline
+          readOnly
+        />
+      </FormField>
+      <FormField>
+        <Text.Label>Date</Text.Label>
+        <Input value={formatDate(txn.timestamp)} readOnly />
+      </FormField>
+      <FormField>
+        <Text.Label>Category</Text.Label>
+        <View style={styles.row}>
+          {categories?.map((val) => (
+            <Chip
+              key={val.id}
+              label={val.name}
+              selected={val.id === state.categoryId}
+              callback={() => handleChange("categoryId", val.id)}
+            />
+          ))}
+        </View>
+      </FormField>
+      <FormField>
+        <Text.Label>Note</Text.Label>
+        <Input
+          placeholder="Additional Note"
+          value={state.note}
+          onChangeText={(text) => handleChange("note", text)}
+          autoCapitalize="words"
+        />
+      </FormField>
+      <FormField>
+        <Text.Label>Status</Text.Label>
+        <View style={styles.row}>
+          {STATUSES.map((status) => (
+            <Chip
+              key={status}
+              label={parsedTxnStatusLabel(status)}
+              selected={state.status === status}
+              callback={() => handleChange("status", status)}
+            />
+          ))}
+        </View>
+      </FormField>
+      <Button onPress={handleSubmit} disabled={isPending}>
+        <Button.Label>Save</Button.Label>
+      </Button>
+    </Form>
   );
 }
 
