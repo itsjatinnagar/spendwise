f59f7e0c23ac580c4dda1a1e7bac94cdfeac0d72
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2026-05-02 11:04:54 +0530

    feat: render user greeting on home tab

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,27 @@
+import Text from "@/components/common/text";
+import { useUser } from "@/hooks/use-user";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Screen() {
-  return <SafeAreaView></SafeAreaView>;
+  const { data, isLoading } = useUser();
+
+  if (isLoading || !data) return;
+
+  return (
+    <SafeAreaView style={styles.container} edges={["left", "right", "top"]}>
+      <View>
+        <Text.Caption>Welcome back,</Text.Caption>
+        <Text.Title>{data.name}</Text.Title>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 24,
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+});
