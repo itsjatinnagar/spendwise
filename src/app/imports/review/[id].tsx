2789ef9cb0d743b8536bc37b8474b4885c024148
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2026-06-10 10:27:32 +0530

    feat: filter parsed transactions by status

--- src/app/imports/review/[id].tsx
+++ src/app/imports/review/[id].tsx
@@ -1,20 +1,26 @@
 import ParsedTxnCard from "@/components/cards/parsed-txn-card";
 import Button from "@/components/common/button";
 import Text from "@/components/common/text";
-import { ParsedTxnStatus } from "@/database/schema";
+import { parsedTxns, ParsedTxnStatus } from "@/database/schema";
 import { useCommit } from "@/hooks/use-commit";
 import { useParsedTxns } from "@/hooks/use-parsed-txns";
+import { parsedTxnStatusLabel } from "@/utilities/lib";
 import { router, useLocalSearchParams } from "expo-router";
-import { FlatList, StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { FlatList, Pressable, ScrollView, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Screen() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const { data } = useParsedTxns(id);
   const { isPending, mutateAsync } = useCommit();
+  const [status, setStatus] = useState<ParsedTxnStatus | null>(null);
 
   const approved =
     data?.filter((val) => val.status === ParsedTxnStatus.APPROVED) ?? [];
+
+  const transactions =
+    status === null ? data : data?.filter((val) => val.status === status);
 
   const handleCommit = async () => {
     await mutateAsync(approved);
@@ -23,38 +29,113 @@ export default function Screen() {
 
   return (
     <SafeAreaView style={styles.container} edges={["bottom", "left", "right"]}>
+      <Header transactions={data} status={status} onSetStatus={setStatus} />
       <FlatList
-        data={data}
+        data={transactions}
         contentContainerStyle={styles.list}
         keyExtractor={({ id }) => id}
         showsVerticalScrollIndicator={false}
         renderItem={({ item }) => <ParsedTxnCard parsedTxn={item} />}
       />
-      <View style={styles.footer}>
-        <Text.Caption style={styles.footerText}>
-          {approved.length} of {data?.length ?? 0} will be committed.
-        </Text.Caption>
-        <Button onPress={handleCommit} disabled={isPending}>
-          <Button.Label>Commit</Button.Label>
-        </Button>
-      </View>
+      <Button onPress={handleCommit} disabled={isPending}>
+        <Button.Label>Commit</Button.Label>
+      </Button>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    gap: 8,
+    gap: 28,
     flex: 1,
     paddingHorizontal: 20,
   },
   list: {
     gap: 16,
   },
-  footer: {
-    gap: 8,
+  header: {
+    flexGrow: 0,
   },
-  footerText: {
-    textAlign: "center",
+  chip: {
+    gap: 8,
+    height: 28,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "#BBBFCC",
+    borderRadius: 14,
+    paddingHorizontal: 12,
   },
 });
+
+function Header({
+  transactions,
+  status,
+  onSetStatus,
+}: {
+  transactions: (typeof parsedTxns.$inferSelect)[] | undefined;
+  status: ParsedTxnStatus | null;
+  onSetStatus: React.Dispatch<React.SetStateAction<ParsedTxnStatus | null>>;
+}) {
+  const statuses = Object.values(ParsedTxnStatus).filter(
+    (value) => typeof value === "number",
+  );
+
+  const renderChip = (value: ParsedTxnStatus) => {
+    const count = transactions?.filter((val) => val.status === value).length;
+    return (
+      <Chip
+        key={value.toString()}
+        active={status === value}
+        count={count}
+        label={parsedTxnStatusLabel(value)}
+        onPress={() => onSetStatus(value)}
+      />
+    );
+  };
+
+  return (
+    <ScrollView
+      style={styles.header}
+      contentContainerStyle={styles.list}
+      showsHorizontalScrollIndicator={false}
+      horizontal
+    >
+      <Chip
+        active={status === null}
+        count={transactions?.length}
+        label="All"
+        onPress={() => onSetStatus(null)}
+      />
+      {statuses.map(renderChip)}
+    </ScrollView>
+  );
+}
+
+type VoidCallback = () => void;
+
+function Chip({
+  active = false,
+  count,
+  label,
+  onPress,
+}: {
+  active?: boolean;
+  count: number | undefined;
+  label: string | undefined;
+  onPress: VoidCallback;
+}) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[
+        styles.chip,
+        active && { backgroundColor: "#FFF7E0", borderColor: "#FFF7E0" },
+      ]}
+    >
+      <Text style={[active && { color: "#FFBA00" }]}>{label}</Text>
+      <Text style={[active && { color: "#FFBA00" }]}>{count}</Text>
+    </Pressable>
+  );
+}
