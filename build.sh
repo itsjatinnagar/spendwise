de765ed2b7c3e070dd590f1d71957880cf93e3ce
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2026-07-04 12:05:39 +0530

    remove: env and build script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
-export ANDROID_HOME="/Users/itsjatinnagar/Library/Android/sdk"
-
-cd android
-./gradlew app:assembleRelease
